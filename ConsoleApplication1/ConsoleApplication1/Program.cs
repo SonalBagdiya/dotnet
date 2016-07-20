541e27f2188a680d5a4cc91e12985a7720b1f9b1
Nagesh Ajab <nagesha@synerzipune.local>
2016-07-20 16:05:43 +0530

    kalo

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication1
 
         {
             
-            Console.WriteLine("hello world");
+            Console.WriteLine("KALO world");
             Console.ReadLine();
         }
     }
